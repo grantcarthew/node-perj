23012b5a77b5c1beb34ccbac418dcdb8369d4203
Grant Carthew <grant@carthew.net>
2018-05-04 17:43:36 +1000

    Benchmark extended

--- benchmark.js
+++ benchmark.js
@@ -7,25 +7,58 @@ const jrep = require('./jrep')
 const pino = require('pino')
 const bench = require('fastbench')
 const fs = require('fs')
-var dest = fs.createWriteStream('/dev/null')
+const hostname = require('os').hostname
+const pid = process.pid
+const dest = fs.createWriteStream('/dev/null')
+const scifi = require('./data-scifi')
 
-const jlog = jrep.create({stream: dest})
+// Adding hostname and pid to match pino log string
+const jlog = jrep.create({hostname, pid, write: dest.write.bind(dest)})
 const plog = pino(dest)
 
 const run = bench([
-  function jrep (done) {
-    jlog.info('hello world')
-    jlog.info({foo: 'bar'})
-    jlog.info({foo: 'bar'}, 'hello world')
+  function jrepCommonOperations (done) {
+    job(jlog)
     setImmediate(done)
   },
-  function pino (done) {
-    plog.info('hello world')
-    plog.info({foo: 'bar'})
-    plog.info({foo: 'bar'}, 'hello world')
+  function pinoCommonOperations (done) {
+    job(plog)
+    setImmediate(done)
+  },
+  function jrepCommonWithError (done) {
+    jobWithError(jlog)
+    setImmediate(done)
+  },
+  function pinoCommonWithError (done) {
+    jobWithError(plog)
+    setImmediate(done)
+  },
+  function jrepCommonWithChild (done) {
+    jobWithChild(jlog)
+    setImmediate(done)
+  },
+  function pinoCommonWithChild (done) {
+    jobWithChild(plog)
     setImmediate(done)
   }
 ], 1000000)
 
+function jobWithError (log) {
+  log.error(new Error(scifi.msg[1]))
+  job(log)
+}
+
+function jobWithChild (log) {
+  let clog = log.child({ foo: 'bar' })
+  job(clog)
+}
+
+function job (log) {
+  log.fatal(scifi.msg[0])
+  log.warn(scifi.dataLarge)
+  log.info(scifi.dataSmall, scifi.msg[0])
+  log.debug(scifi.dataMedium, scifi.msg[0])
+  log.trace(scifi.dataLarge, scifi.msg[0])
+}
 // run them two times
 run(run)

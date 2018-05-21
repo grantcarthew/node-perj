2470a47760fd188764ebb641b942640ee7c49bfd
Grant Carthew <grant@carthew.net>
2018-05-22 08:14:57 +1000

    Renamed Symbols

--- src/perj.js
+++ src/perj.js
@@ -3,16 +3,17 @@ const serializerr = require('./serializerr')
 const defaultOptions = require('./options')
 const dateTimeFunctions = require('./date-time')
 
-const symSplitOptions = Symbol('SplitOptions')
-const symOptions = Symbol('Options')
-const symTopString = Symbol('TopString')
-const symTopStringSnip = Symbol('TopStringPart')
-const symTopCache = Symbol('TopCache')
-const symHeaders = Symbol('Headers')
-const symHeaderStrings = Symbol('Headers')
-const symHeaderObjects = Symbol('Headers')
-const symAddLogHeader = Symbol('AddLogHeader')
-const symAddLogFunction = Symbol('AddLogFunction')
+// Symbols for functions and values
+const _SplitOptions = Symbol('SplitOptions')
+const _Options = Symbol('Options')
+const _TopString = Symbol('TopString')
+const _TopStringSnip = Symbol('TopStringPart')
+const _TopCache = Symbol('TopCache')
+const _Headers = Symbol('Headers')
+const _HeaderStrings = Symbol('Headers')
+const _HeaderObjects = Symbol('Headers')
+const _AddLogHeader = Symbol('AddLogHeader')
+const _AddLogFunction = Symbol('AddLogFunction')
 
 // TODO: Remove this line.
 require('console-probe').apply()
@@ -26,49 +27,49 @@ module.exports = Object.freeze({
 
 class Perj {
   constructor (options) {
-    this[symOptions] = Object.assign({}, defaultOptions)
-    this[symTopString] = ''
-    this[symTopStringSnip] = {}
-    this[symTopCache] = {}
-    this[symSplitOptions](options)
-    this[symHeaders] = {}
-    this[symHeaderStrings] = {}
-    this[symHeaderObjects] = {}
-    for (const level in this[symOptions].levels) {
-      this[symAddLogHeader](level)
-      this[symAddLogFunction](level)
+    this[_Options] = Object.assign({}, defaultOptions)
+    this[_TopString] = ''
+    this[_TopStringSnip] = {}
+    this[_TopCache] = {}
+    this[_SplitOptions](options)
+    this[_Headers] = {}
+    this[_HeaderStrings] = {}
+    this[_HeaderObjects] = {}
+    for (const level in this[_Options].levels) {
+      this[_AddLogHeader](level)
+      this[_AddLogFunction](level)
     }
   }
 
   get level () {
-    return this[symOptions].level
+    return this[_Options].level
   }
 
   set level (level) {
-    if (!(this[symOptions].levels.hasOwnProperty(level))) {
+    if (!(this[_Options].levels.hasOwnProperty(level))) {
       throw new Error('The level option must be a valid key in the levels object.')
     }
-    this[symOptions].level = level
+    this[_Options].level = level
   }
 
   get levels () {
-    return this[symOptions].levels
+    return this[_Options].levels
   }
 
   addLevel (newLevels) {
     for (const level in newLevels) {
       if (this[level]) { continue }
-      this[symOptions].levels[level] = newLevels[level]
-      this[symAddLogHeader](level)
-      this[symAddLogFunction](level)
+      this[_Options].levels[level] = newLevels[level]
+      this[_AddLogHeader](level)
+      this[_AddLogFunction](level)
     }
   }
 
   get write () {
-    return this[symOptions].write
+    return this[_Options].write
   }
 
-  [symSplitOptions] (options) {
+  [_SplitOptions] (options) {
     if (!options) { return }
     for (const key in options) {
       if (defaultOptions.hasOwnProperty(key)) {
@@ -76,50 +77,50 @@ class Perj {
           this.level = options[key]
           continue
         }
-        this[symOptions][key] = options[key]
+        this[_Options][key] = options[key]
       } else {
         const snip = ',"' + key + '":' + stringifyTopValue(options[key])
-        this[symTopString] += snip
-        this[symTopStringSnip][key] = snip
-        this[symTopCache][key] = options[key]
+        this[_TopString] += snip
+        this[_TopStringSnip][key] = snip
+        this[_TopCache][key] = options[key]
       }
     }
   }
 
-  [symAddLogHeader] (level) {
-    this[symHeaderStrings][level] = '{"' +
-      this[symOptions].levelKey + '":"' + level + '","' +
-      this[symOptions].levelNumberKey + '":' + this[symOptions].levels[level] +
-      this[symTopString] + ',"' +
-      this[symOptions].dateTimeKey + '":'
-    if (this[symOptions].passThrough) {
-      this[symHeaderObjects][level] = Object.assign({
-        [this[symOptions].levelKey]: level,
-        [this[symOptions].levelNumberKey]: this[symOptions].levels[level]
-      }, this[symTopCache])
+  [_AddLogHeader] (level) {
+    this[_HeaderStrings][level] = '{"' +
+      this[_Options].levelKey + '":"' + level + '","' +
+      this[_Options].levelNumberKey + '":' + this[_Options].levels[level] +
+      this[_TopString] + ',"' +
+      this[_Options].dateTimeKey + '":'
+    if (this[_Options].passThrough) {
+      this[_HeaderObjects][level] = Object.assign({
+        [this[_Options].levelKey]: level,
+        [this[_Options].levelNumberKey]: this[_Options].levels[level]
+      }, this[_TopCache])
     }
   }
 
-  [symAddLogFunction] (level) {
+  [_AddLogFunction] (level) {
     this[level] = function (...items) {
-      if (this[symOptions].levels[this[symOptions].level] >
-        this[symOptions].levels[level]) {
+      if (this[_Options].levels[this[_Options].level] >
+        this[_Options].levels[level]) {
         return
       }
-      const time = this[symOptions].dateTimeFunction()
+      const time = this[_Options].dateTimeFunction()
       const splitItems = stringifyLogItems(items)
-      const json = this[symHeaderStrings][level] + time +
-          ',"' + this[symOptions].messageKey + '":"' + splitItems.msg +
-          '","' + this[symOptions].dataKey + '":' + splitItems.dataStr + '}\n'
-      if (this[symOptions].passThrough) {
-        const obj = Object.assign(this[symHeaderObjects][level], {
-          [this[symOptions].dateTimeKey]: time,
-          [this[symOptions].messageKey]: splitItems.msg,
-          [this[symOptions].dataKey]: splitItems.data
+      const json = this[_HeaderStrings][level] + time +
+          ',"' + this[_Options].messageKey + '":"' + splitItems.msg +
+          '","' + this[_Options].dataKey + '":' + splitItems.dataStr + '}\n'
+      if (this[_Options].passThrough) {
+        const obj = Object.assign(this[_HeaderObjects][level], {
+          [this[_Options].dateTimeKey]: time,
+          [this[_Options].messageKey]: splitItems.msg,
+          [this[_Options].dataKey]: splitItems.data
         })
-        this[symOptions].write(json, obj)
+        this[_Options].write(json, obj)
       } else {
-        this[symOptions].write(json)
+        this[_Options].write(json)
       }
     }
   }
@@ -129,33 +130,33 @@ class Perj {
       throw new Error('Provide top level arguments to create a child logger.')
     }
     const newChild = Object.create(this)
-    newChild[symTopStringSnip] = Object.assign({}, this[symTopStringSnip])
-    newChild[symTopCache] = Object.assign({}, this[symTopCache])
+    newChild[_TopStringSnip] = Object.assign({}, this[_TopStringSnip])
+    newChild[_TopCache] = Object.assign({}, this[_TopCache])
     for (const key in tops) {
       if (defaultOptions.hasOwnProperty(key)) { continue }
-      if (this[symTopCache].hasOwnProperty(key) &&
-            isString(this[symTopCache][key]) &&
+      if (this[_TopCache].hasOwnProperty(key) &&
+            isString(this[_TopCache][key]) &&
             isString(tops[key])) {
-        const snip = this[symTopCache][key] + this[symOptions].separator + tops[key]
-        newChild[symTopCache][key] = snip
-        newChild[symTopStringSnip][key] = ',"' + key + '":"' + snip + '"'
+        const snip = this[_TopCache][key] + this[_Options].separator + tops[key]
+        newChild[_TopCache][key] = snip
+        newChild[_TopStringSnip][key] = ',"' + key + '":"' + snip + '"'
       } else {
-        newChild[symTopCache][key] = tops[key]
-        newChild[symTopStringSnip][key] = ',"' + key + '":' + stringifyTopValue(tops[key])
+        newChild[_TopCache][key] = tops[key]
+        newChild[_TopStringSnip][key] = ',"' + key + '":' + stringifyTopValue(tops[key])
       }
     }
-    newChild[symTopString] = ''
-    for (const key in newChild[symTopStringSnip]) {
-      newChild[symTopString] += newChild[symTopStringSnip][key]
+    newChild[_TopString] = ''
+    for (const key in newChild[_TopStringSnip]) {
+      newChild[_TopString] += newChild[_TopStringSnip][key]
     }
     newChild.parent = this
-    newChild[symOptions] = Object.assign({}, this[symOptions])
-    newChild[symHeaderStrings] = Object.assign({}, this[symHeaderStrings])
-    if (this[symOptions].passThrough) {
-      newChild[symHeaderObjects] = Object.assign({}, this[symHeaderObjects])
+    newChild[_Options] = Object.assign({}, this[_Options])
+    newChild[_HeaderStrings] = Object.assign({}, this[_HeaderStrings])
+    if (this[_Options].passThrough) {
+      newChild[_HeaderObjects] = Object.assign({}, this[_HeaderObjects])
     }
-    for (const level in this[symOptions].levels) {
-      newChild[symAddLogHeader](level)
+    for (const level in this[_Options].levels) {
+      newChild[_AddLogHeader](level)
     }
     return newChild
   }

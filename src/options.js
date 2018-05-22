3ff46f410e201de78e86fdc4bcf10c216db8749f
Grant Carthew <grant@carthew.net>
2018-05-22 16:43:37 +1000

    Changed default separator

--- src/options.js
+++ src/options.js
@@ -16,7 +16,7 @@ module.exports = {
   dateTimeFunction: dateTimeFunctions.epoch,
   messageKey: 'msg',
   dataKey: 'data',
-  separator: ' > ',
+  separator: ':',
   passThrough: false,
   write: defaultWriter()
 }
